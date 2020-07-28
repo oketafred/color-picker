616b20a1922d865c5f191ef956e2d895c07e28f9
Oketa Fred <oketafred@gmail.com>
2020-07-28 22:43:10 +0300

    Added More Colors Codes

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,172 @@ export default {
   data() {
     return {
       colors: [	
+        // "#C91F37",,
+        "#DC3023",
+        // COCHINEAL RED
+        "#9D2933",
+        // MONZA
+        "#CF000F",
+        // BREWED MUSTARD-BROWN
+        "#E68364",
+        // POMEGRANATE
+        "#F22613",
+        // SCARLET
+        "#CF3A24",
+        // PURE CRIMSON
+        "#C3272B",
+        // TRUE RED
+        "#8F1D21",
+        // CHESTNUT ROSE
+        "#D24D57",
+        // ONE KIN DYE
+        "#F08F90",
+        // PEACH-COLORED
+        "#F47983",
+        // RED PLUM COLORED
+        "#DB5A6B",
+        // MEDIUM CRIMSON
+        "#C93756",
+        // CHERRY BLOSSOM COLOR
+        "#FCC9B9",
+        // WASHED-OUT CRIMSON
+        "#FFB3A7",
+        // RADICAL RED
+        "#F62459",
+        // IBIS WING COLOR
+        "#F58F84",
+        // WISTERIA PURPLE
+        "#875F9A",
+        // BELLFLOWER COLOR
+        "#5D3F6A",
+        // WISTERIA COLOR
+        "#89729E",
+        // IRIS COLOR
+        "#763568",
+        // TATARIAN ASTER COLOR
+        "#8D608C",
+        // THIN COLOR
+        "#A87CA0",
+        // VIOLET COLOR
+        "#5B3256",
+        // MEDIUM PURPLE
+        "#BF55EC",
+        // STUDIO
+        "#8E44AD",
+        // WISTERIA
+        "#9B59B6",
+        // LIGHT WISTERIA
+        "#BE90D4",
+        // SKY COLOR
+        "#4D8FAC",
+        // ULTRAMARINE COLOR
+        "#5D8CAE",
+        // PICTON BLUE
+        "#22A7F0",
+        // DODGER BLUE
+        "#19B5FE",
+        // PICTON BLUE
+        "#59ABE3",
+        // LIGHT BLUE COLOR
+        "#48929B",
+        // THOUSAND HERB
+        "#317589",
+        // JORDY BLUE
+        "#89C4F4",
+        // STEEL BLUE
+        "#4B77BE",
+        // LAPIS LAZULI COLOR
+        "#1F4788",
+        // NAVY BLUE COLOR
+        "#003171",
+        // HANADA
+        "#044F67",
+        // INDIGO COLOR
+        "#264348",
+        // SISKIN SPROUT YELLOW
+        "#7A942E",
+        // YOUNG GREEN ONION
+        "#8DB255",
+        // FRESH ONION
+        "#5B8930",
+        // YOUNG BAMBOO COLOR
+        "#6B9362",
+        // PATINA
+        "#407A52",
+        // GREEN BAMBOO
+        "#006442",
+        // GOSSIP
+        "#87D37C",
+        // EUCALYPTUS
+        "#26A65B",
+        // JUNGLE GREEN
+        "#26C281",
+        // OBSERVATORY
+        "#049372",
+        // JUNGLE GREEN
+        "#2ABB9B",
+        // MOUNTAIN MEADOW
+        "#16A085",
+        // TURQUOISE
+        "#36D7B7",
+        // FREE SPEECH AQUAMARINE
+        "#03A678",
+        // OCEAN GREEN
+        "#4DAF7C",
+        // PATRINIA FLOWERS
+        "#D9B611",
+        // AMUR CORK TREE
+        "#F3C13A",
+        // RIPE LEMON
+        "#F7CA18",
+        // JAPANESE TRIANDRA GRASS
+        "#E2B13C",
+        // RAPESEED OIL-COLORED
+        "#A17917",
+        // CREAM CAN
+        "#F5D76E",
+        // SAFFRON
+        "#F4D03F",
+        // BRIGHT GOLDEN YELLOW
+        "#FFA400",
+        // SUMAC-DYED
+        "#E08A1E",
+        // GAMBOGE
+        "#FFB61E",
+        // CORN-COLORED
+        "#FAA945",
+        // EGG-COLORED
+        "#FFA631",
+        // FLORAL LEAF-COLORED
+        "#FFB94E",
+        // GOLDEN FALLEN LEAVES
+        "#E29C45",
+        // ECSTASY
+        "#F9690E",
+        // AMBER
+        "#CA6924",
+        // LIGHTNING YELLOW
+        "#F5AB35",
+        // SILVER
+        "#BFBFBF",
+        // CARARRA
+        "#F2F1EF",
+        // SILVER SAND
+        "#BDC3C7",
+        // PORCELAIN
+        "#ECF0F1",
+        // PUMICE
+        "#D2D7D3",
+        // HARBOR RAT
+        "#757D75",
+        // GALLERY
+        "#EEEEEE",
+        // EDWARD
+        "#ABB7B7",
+        // LYNCH
+        "#6C7A89",
+        // CASCADE
+        "#95A5A6",
         // Black & White
         "#000000",
         "#FFFFFF",
@@ -141,7 +307,7 @@ body {
   padding-top: 3.5rem;
 }
 .navbar {
-	background-color: #319795 !important;
+	background-color: rgba(255,255,255,.9) !important;
 }
 .color-box {
   width: 20%;
