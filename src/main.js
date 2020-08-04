6853a46bd8e17a58e8b19a4d74abdbe1ea2f30a8
Nyeko Walter <walter.nyeko.extrenal@atos.net>
2020-08-04 13:46:23 +0300

    Added Clipboard Package

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+import Clipboard from 'v-clipboard'
+
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 
@@ -10,6 +12,8 @@ Vue.component(
 );
 
 Vue.config.productionTip = false;
+
+Vue.use(Clipboard)
 
 new Vue({
   render: (h) => h(App),
